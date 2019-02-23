86f94f2a02ba204fa61f8a65b92c13ec1aae3608
Yas Okada <yas.okadatech@gmail.com>
2019-02-23 13:48:31 +0900

    (wip) wiht_template

--- app/mailers/send_grid_mailer.rb
+++ app/mailers/send_grid_mailer.rb
@@ -4,4 +4,17 @@ class SendGridMailer < ApplicationMailer
   def hello
     mail(subject: 'hello', to: ENV['SENGDRID_TO'])
   end
+
+  def with_template
+    xsmtp_api_params = {
+      personalizations: {
+        dynamic_template_data: {
+          name: 'Alice'
+        }
+      },
+      template_id: ENV['SENDGRID_TEMPLATE_ID']
+    }
+    headers['X-SMTPAPI'] = xsmtp_api_params.to_json
+    mail(subject: 'hello', body: 'test', to: ENV['SENGDRID_TO'])
+  end
 end
