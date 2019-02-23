b9e294a546b8af904c966bf61477b19cb52dc7f3
Yas Okada <yas.okadatech@gmail.com>
2019-02-23 20:35:08 +0900

    template with SMTP

--- app/mailers/send_grid_mailer.rb
+++ app/mailers/send_grid_mailer.rb
@@ -5,16 +5,44 @@ class SendGridMailer < ApplicationMailer
     mail(subject: 'hello', to: ENV['SENGDRID_TO'])
   end
 
-  def with_template
+  def by_template
+    # TODO: あとで Web API v3 で使う
+    # xsmtp_api_params = {
+    #     from: {
+    #         email: 'sample_sendgrid@example.com'
+    #     },
+    #     personalizations: [
+    #         {
+    #             to: [
+    #                 { enail: ENV['SENGDRID_TO'] }
+    #             ],
+    #             dynamic_template_data: {
+    #                 name: 'Alice'
+    #             },
+    #         }
+    #     ],
+    #     template_id: ENV['SENDGRID_TEMPLATE_ID']
+    # }
     xsmtp_api_params = {
-      personalizations: {
-        dynamic_template_data: {
-          name: 'Alice'
-        }
+      to: [
+        ENV['SENGDRID_TO']
+      ],
+      sub: {
+        ':name': [
+          'Alice'
+        ]
       },
-      template_id: ENV['SENDGRID_TEMPLATE_ID']
+      filters: {
+        templates: {
+          settings: {
+            enable: 1,
+            template_id: ENV['SENDGRID_LEGACY_TEMPLATE_ID']
+          }
+        }
+      }
     }
+
     headers['X-SMTPAPI'] = xsmtp_api_params.to_json
-    mail(subject: 'hello', body: 'test', to: ENV['SENGDRID_TO'])
+    mail(subject: 'hello', body: '', to: ENV['SENGDRID_TO_DUMMY'])
   end
 end
