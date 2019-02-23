9cc492c47588a71dcb5c73e1975858b83d048e9b
Yas Okada <yas.okadatech@gmail.com>
2019-02-23 13:23:34 +0900

    initial code of sending via SendGrid

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -37,6 +37,17 @@ Rails.application.configure do
 
   config.action_mailer.perform_caching = false
 
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = {
+    address: 'smtp.sendgrid.net',
+    port: 587,
+    domain: ENV['SENDGRID_DOMAIN'],
+    user_name: ENV['SENDGRID_USERNAME'],
+    password: ENV['SENDGRID_PASSWORD'],
+    authentication: :plain,
+    enable_starttls_auto: true
+  }
+
   # Print deprecation notices to the Rails logger.
   config.active_support.deprecation = :log
 
